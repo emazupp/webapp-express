99807d6f65044b880c6133450c456a1dffb416c3
emazupp <zuppardo.emanuele2001@gmail.com>
2025-01-10 15:58:25 +0100

    bonus

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,9 +1,47 @@
+const connection = require("../data/config");
+const { APP_HOST, APP_PORT } = process.env;
+
 function index(req, res) {
-  res.json({ message: "index" });
+  const sql =
+    "SELECT id, title, director, genre, release_year, image FROM movies";
+
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
+
+      return res.status(500).json({
+        status: "KO",
+        message: "Query failed",
+      });
+    }
+
+    res.json(correctImagePath(results));
+  });
 }
 
 function show(req, res) {
-  res.json({ message: "show" });
+  const movieID = req.params.id;
+  const sql = "SELECT * FROM movies WHERE movies.id = ?";
+
+  connection.query(sql, [movieID], (err, results) => {
+    if (err) {
+      console.log(err);
+
+      return res.status(500).json({
+        status: "KO",
+        message: "Query failed",
+      });
+    }
+
+    res.json(correctImagePath(results));
+  });
+}
+
+function correctImagePath(movies) {
+  return movies.map((movie) => ({
+    ...movie,
+    image: `http://${APP_HOST}:${APP_PORT}/movies_cover/${movie.image}`,
+  }));
 }
 
 module.exports = { index, show };
--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const moviesController = require("../controllers/moviesController");
 
 router.get("/", moviesController.index);
-router.get("/:id", moviesController.index);
+router.get("/:id", moviesController.show);
 
 module.exports = router;
